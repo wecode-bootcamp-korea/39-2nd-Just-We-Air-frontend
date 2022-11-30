e2f019db6c657d593f581592f39179323d2dee97
AliceNam93 <yeonwood93@gmail.com>
2022-11-30 14:16:32 +0900

    라우터에 루트 추가. 컨피그 파일 추가

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const BASE_URL = 'API주소-백엔드제공';
+const API = {
+  main: `${BASE_URL}/`,
+  booking: `${BASE_URL}/booking`,
+  login: `${BASE_URL}/log-in`,
+  signup: `${BASE_URL}/sign-up`,
+  payment: `${BASE_URL}/payment`,
+};
+export default API;
