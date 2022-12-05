b2932118d4806e1c741935f90f1f72e51fce6d62
AliceNam93 <yeonwood93@gmail.com>
2022-12-05 17:24:37 +0900

    라우터 js에 페이먼트썩세스  페이지 추가

--- src/Router.js
+++ src/Router.js
@@ -9,6 +9,7 @@ import Payment from './pages/Payment/Payment';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 import RedirectHandler from './pages/Login/RedirectHandler';
+import PaymentSuccess from './pages/Payment/PaymentSuccess';
 
 export default function Router() {
   return (
@@ -21,6 +22,7 @@ export default function Router() {
         <Route path="/mobile-ticket" element={<MobileTicket />} />
         <Route path="/oauth/callback/kakao" element={<RedirectHandler />} />
         <Route path="/payment" element={<Payment />} />
+        <Route path="/payment-success" element={<PaymentSuccess />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
       <Footer />
---
+++ src/pages/Payment/PaymentSuccess.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function PaymentSuccess() {
+  return <h1>Payment Success Page</h1>;
+}
